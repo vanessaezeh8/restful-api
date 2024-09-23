3e92a8aab1936d0706d049d9b81779d250bd3e07
Vanessa Ezeh <vanessaezeh8@gmail.com>
2024-09-23 00:28:56 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return{"Hello World"}
+    
+if __name__ == "__main__":
+    app.run(debug=True)
