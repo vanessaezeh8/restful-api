aa32712cd21d2d12cc41d8e1c98e3d735ee8fa35
Vanessa Ezeh <vanessaezeh8@gmail.com>
2024-09-25 19:54:00 -0500

    Handling the Test JSON file

--- test.py
+++ test.py
@@ -2,4 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = 
+data = [{"likes":78, "name":"Joe", "views":100000},
+        {"likes" : 10000, "name": "How to make REST API", "views": 80000},
+        {"likes" : 35, "name": "Tim", "views": 2000}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.get(BASE + "video/2")
+print(response.json())
