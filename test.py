a785a8227b91c90e9bb8645f78a920fe02476309
Vanessa Ezeh <vanessaezeh8@gmail.com>
2024-09-26 22:00:18 -0500

    Tested the response

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.patch(BASE + "video/2", {"views":99})
+response = requests.patch(BASE + "video/2", {"views":99,"likes":101})
 print(response.json())
